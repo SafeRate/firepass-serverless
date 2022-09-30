7850c75f4dc96dae10919f1ca8aa62de6145e832
Dylan Hall <dylan@perseeventures.com>
2022-09-30 17:33:00 -0500

    updated serverless

--- utils/estated.ts
+++ utils/estated.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { env } from "./env";
 
+// https://estated.com/developers/docs/v4/property/values
+
 export type EstatedProperty = {
   address: EstatedPropertyAddress;
   assessments: EstatedPropertyAssessment[];
